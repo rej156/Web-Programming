5f3c6b5ec0aea09f2eaf62b7e82680a521b8e4c0
Eric John Juta <ericjohnjuta@gmail.com>
2015-02-06 14:38:34 +0000

    Initial commit

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php // Example 26-12: logout.php
+  require_once 'header.php';
+
+  if (isset($_SESSION['user']))
+  {
+    destroySession();
+    echo "<div class='main'>You have been logged out. Please " .
+         "<a href='index.php'>click here</a> to refresh the screen.<br><br></div>";
+  }
+  else echo "<div class='main'><br>You cannot log out because you are not logged in<br><br></div>";
+?>
+  </body>
+</html>
